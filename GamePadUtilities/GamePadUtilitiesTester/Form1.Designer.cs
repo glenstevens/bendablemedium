7d3faaa16c0f5eca0e6efa0e7a9f8217a910ffab
mglenstevens <mglenstevens@1c5b149b-b7e2-4aec-38bb-94352d0170ee>
2010-07-24 14:56:57 +0000

    Initial upload

---
+++ GamePadUtilities/GamePadUtilitiesTester/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace GamePadUtilitiesTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLeftTrigger = new System.Windows.Forms.TextBox();
+            this.txtLeftThumbstick = new System.Windows.Forms.TextBox();
+            this.txtRightTrigger = new System.Windows.Forms.TextBox();
+            this.txtRightThumbstick = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ButtonOutputListBox = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Left Trigger";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Right Trigger";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Left Thumbstick";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Buttons";
+            // 
+            // txtLeftTrigger
+            // 
+            this.txtLeftTrigger.Location = new System.Drawing.Point(8, 103);
+            this.txtLeftTrigger.Name = "txtLeftTrigger";
+            this.txtLeftTrigger.Size = new System.Drawing.Size(179, 26);
+            this.txtLeftTrigger.TabIndex = 4;
+            // 
+            // txtLeftThumbstick
+            // 
+            this.txtLeftThumbstick.Location = new System.Drawing.Point(8, 172);
+            this.txtLeftThumbstick.Name = "txtLeftThumbstick";
+            this.txtLeftThumbstick.Size = new System.Drawing.Size(179, 26);
+            this.txtLeftThumbstick.TabIndex = 5;
+            // 
+            // txtRightTrigger
+            // 
+            this.txtRightTrigger.Location = new System.Drawing.Point(193, 103);
+            this.txtRightTrigger.Name = "txtRightTrigger";
+            this.txtRightTrigger.Size = new System.Drawing.Size(179, 26);
+            this.txtRightTrigger.TabIndex = 6;
+            // 
+            // txtRightThumbstick
+            // 
+            this.txtRightThumbstick.Location = new System.Drawing.Point(193, 172);
+            this.txtRightThumbstick.Name = "txtRightThumbstick";
+            this.txtRightThumbstick.Size = new System.Drawing.Size(179, 26);
+            this.txtRightThumbstick.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Right Thumbstick";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 24);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Controller 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(188, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 24);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.Text = "Controller 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 33);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 24);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.Text = "Controller 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(188, 33);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(115, 24);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.Text = "Controller 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Location = new System.Drawing.Point(8, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 65);
+            this.panel1.TabIndex = 14;
+            // 
+            // ButtonOutputListBox
+            // 
+            this.ButtonOutputListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonOutputListBox.FormattingEnabled = true;
+            this.ButtonOutputListBox.ItemHeight = 20;
+            this.ButtonOutputListBox.Location = new System.Drawing.Point(12, 237);
+            this.ButtonOutputListBox.Name = "ButtonOutputListBox";
+            this.ButtonOutputListBox.Size = new System.Drawing.Size(360, 344);
+            this.ButtonOutputListBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 599);
+            this.Controls.Add(this.ButtonOutputListBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRightThumbstick);
+            this.Controls.Add(this.txtRightTrigger);
+            this.Controls.Add(this.txtLeftThumbstick);
+            this.Controls.Add(this.txtLeftTrigger);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(407, 654);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLeftTrigger;
+        private System.Windows.Forms.TextBox txtLeftThumbstick;
+        private System.Windows.Forms.TextBox txtRightTrigger;
+        private System.Windows.Forms.TextBox txtRightThumbstick;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox ButtonOutputListBox;
+    }
+}
+
